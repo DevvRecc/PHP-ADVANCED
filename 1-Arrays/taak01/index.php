83d9128c3111a14069cfeb651a222887e3b1230b
Recep Yildiz <recep.yildiz@student.rocva.nl>
2022-01-10 10:15:58 +0100

    taak01 afgemaakt

---
+++ 1-Arrays/taak01/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<body>
+
+<?php
+
+$studenten['Piet'] = "Piet";
+$studenten['Mohamed'] = "Mohamed";
+$studenten['Kees'] = "Kees";
+echo "Voornaam: ". $studenten['Mohamed'] . "</br>";
+
+$achternamen['Piet'] = "Van Gooij";
+$achternamen['Mohamed'] = "Ali";
+$achternamen['Kees'] = "Munt";
+echo "Achternaam: ". $achternamen['Mohamed'] . "</br>";
+
+$leeftijd['Piet'] = 16;
+$leeftijd['Mohamed'] = 17;
+$leeftijd['Kees'] = 19;
+echo "Leeftijd: ". $leeftijd['Mohamed'] . "</br>";
+
+$klas['Piet'] = "8g";
+$klas['Mohamed'] = "6g";
+$klas['Kees'] = "8g";
+echo "Klas: ". $klas['Mohamed'] . "</br>";
+
+?>
+    
+</body>
+</html>
