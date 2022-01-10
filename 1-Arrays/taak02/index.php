306bf12a31e2d83e42485b6a409eca8fa47a03a4
Recep Yildiz <recep.yildiz@student.rocva.nl>
2022-01-10 14:16:50 +0100

    1.2 afgemaakt

---
+++ 1-Arrays/taak02/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<body>
+    
+<?php 
+
+$persoon["voornaam"] = "Willem Alexander";
+$persoon["achternaam"] = "Van Oranje";
+$persoon["leeftijd"] = "53";
+$persoon["opleiding"] = "Software Ontwikkeling";
+
+echo $persoon['voornaam']. " " . $persoon['achternaam'] . " is " . $persoon['leeftijd'] . " jaar oud en is student aan het ROC vam Amsterdam aan de opleiding" . " " . $persoon['opleiding'] . "."; 
+
+?>
+
+</body>
+</html>
