0d1e0a1c9d559306e021d0473b57590685ec90ce
Recep Yildiz <recep.yildiz@student.rocva.nl>
2022-01-10 14:38:18 +0100

    2.1 afgemaakt

---
+++ 2-Loops/taak01/loops-while.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<body>
+    
+<?php 
+
+$leeftijd = 0;
+
+while ($leeftijd < 18){
+
+    echo "ik ben $leeftijd jaar, dus ik mag nog niet stemmen <br>";
+    $leeftijd++;
+}
+    echo "ik ben 18 jaar, dus ik heb stemrecht!";
+
+?>
+
+</body>
+</html>
