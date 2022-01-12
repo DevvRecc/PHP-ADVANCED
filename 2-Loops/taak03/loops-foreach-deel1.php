1bb6ccb37e2f19b3c1a9167e196576f27f4ff1e5
Recep Yildiz <recep.yildiz@student.rocva.nl>
2022-01-12 20:14:57 +0100

    2.3 afgemaakt

---
+++ 2-Loops/taak03/loops-foreach-deel1.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<body>
+
+<?php 
+    echo "<ul>";
+$dagen = array("Maandag", "Dinsdag", "Woensdag", "Donderdag", "Vrijdag", "Zaterdag", "Zondag");
+    foreach($dagen as $day)
+    echo "<li>$day <br> </li>";
+    echo "</ul>";
+?>
+    
+</body>
+</html>
