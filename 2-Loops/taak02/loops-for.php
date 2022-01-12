28554fa566173ed6f1c0067e3b618e4924356258
Recep Yildiz <recep.yildiz@student.rocva.nl>
2022-01-12 19:28:46 +0100

    2.2 afgemaakt

--- 2-Loops/taak02/loops-for.php
+++ 2-Loops/taak02/loops-for.php
@@ -2,7 +2,12 @@
 <body>
     
 <?php 
-
+    echo "<ul>";
+for($x = 0 ; $x < 18; $x++){
+    echo "Ik ben $x jaar jong, dus ik mag nog niet stemmen. <br>";
+}
+    echo "Ik ben 18 jaar oud, dus ik heb stemrecht!";
+    echo "</ul>";
 ?>
 
 </body>
