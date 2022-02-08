1ac4dc020b5b716fbcd4630e19d69441b55450d8
Recep Yildiz <recep.yildiz@student.rocva.nl>
2022-02-08 09:45:42 +0100

    3.2 afgemaakt

---
+++ 3-Forms/taak02/mijngegevens.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Gegevens</title>
+</head>
+<body>
+
+<?php 
+    echo "Mijn Voornaam: " .$_POST["voornaam"] . "</br>";
+    echo "Mijn Achternaam: " .$_POST["achternaam"] . "</br>";
+    echo "Mijn Geslacht: " .$_POST["geslacht"] . "</br>";
+    echo "Mijn Leeftijd: " .$_POST["leeftijd"] . "</br>";
+?>
+
+</body>
+</html>
