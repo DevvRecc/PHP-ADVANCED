d5e1cb64f0b29eecd2be80afa9c31fa7094fe224
Recep Yildiz <recep.yildiz@student.rocva.nl>
2022-02-10 10:31:04 +0100

    3.4 afgemaakt

---
+++ 3-Forms/taak03/gegevens.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Gegevens</title>
+</head>
+<body>
+    
+<?php
+
+    echo "Artiest: " .$_GET["artiest"] . "</br>";
+    echo "Titel: " .$_GET["titel"] . "</br>";
+    echo "Jaar van Uitkomst: " .$_GET["jaar"] . "</br>";
+    echo "Muzieksoort: " .$_GET["muzieksoort"] . "</br>";  
+
+?>
+
+</body>
+</html>
---
+++ 3-Forms/taak03/mijnmuziek.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Invoer</title>
+</head>
+<body>
+    <form name="mijnmuziek" method="GET" action="gegevens.php"> 
+        <p>Artiest: <input name="artiest"></p>
+        <p>Titel: <input name="titel"></p>
+        <p>Jaar van uitkomst: <input name="jaar"></p>
+        <p>Muzieksoort: <input name="muzieksoort"></p>
+        
+        </select>
+        <input type="submit" name="submit" value="Verstuur">
+    </form>    
+    
+</body>
+</html>
