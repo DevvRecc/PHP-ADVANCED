0e18538beced68b046f1b3037347e89a8c3e76f8
Recep Yildiz <recep.yildiz@student.rocva.nl>
2022-02-17 10:18:01 +0100

    Eindopdracht eerste deel

--- opdracht/advanced-1.php
+++ opdracht/advanced-1.php
@@ -7,10 +7,41 @@
     <title>Document</title>
 </head>
 <body>
-    
-<?php 
 
-?>
+<form name="Forum" method="POST" action="advanced-2.php">
+
+<p>Tekst kleur: 
+    <select id="kleur" name="color"></p>
+        <?php 
+        $kleuren = ['red' , 'blue' , 'green' , 'black' , 'brown'];
+        foreach($kleuren as $kleur){
+            echo "<option value='$kleur' selected>$kleur</option>";
+        }
+        ?>
+    </select></p>
+
+<p>Achtergrond Kleur: 
+    <select id="kleur" name="kleurGoal">
+            <?php 
+            $lievelingskleuren = ['blue' , 'red' , 'yellow' , 'green'];
+            foreach($lievelingskleuren as $lievelingskleur){
+                 echo "<option value= '$lievelingskleur' selected>$lievelingskleur</option>";
+            }
+            ?>
+    </select></p>
+
+    <p>Naam: <input name="name"></p>
+    <p>Achternaam: <input name="achternaam"></p>
+    <p>Leeftijd: <input name="leeftijd"></p>
+    <p>Woonplaats: <input name="plaats"></p>
+    <p>Hobby: <input name="hobby"></p>
+    <p>Tabel-Border Dikte: <input name="border-dikte"></p>
+    <p>Padding: <input name="padding"></p>
+
+
+    <input type="submit" name="submit" value="Verstuur">
+
+</form>
 
 </body>
 </html>
--- opdracht/advanced-2.php
+++ opdracht/advanced-2.php
@@ -1,16 +1,31 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
 <?php 
+
+    echo "<body style='background-color:".$_POST['kleurGoal']."'>";
 
 ?>
 
-</body>
-</html>
+<?php
+
+    function maakRij($test, $KL){
+        echo "<tr>";
+        echo "<td style='border: ".$_POST['border-dikte']."px solid; color:".$_POST['color']."; padding:".$_POST['padding']."px;'>$test</td>";
+        echo "<td style='border: ".$_POST['border-dikte']."px solid; color:".$_POST['color']."; padding:".$_POST['padding']."px;'>$KL</td>";
+        echo "</tr>";
+    }
+
+    $informatie = [
+        'Naam'      => $_POST['name'],
+        'Achternaam'      => $_POST['achternaam'],
+        'Leeftijd'      => $_POST['leeftijd'],
+        'Plaats'      => $_POST['plaats'],       
+        'Hobby'      => $_POST['hobby'],
+    ];
+
+    echo "<table style='border:  ".$_POST['border-dikte']."px solid' width='300' cellspacing='0'>";
+
+    foreach( $informatie as $key => $rec ){
+         maakRij($key, $rec);
+    }
+    echo "</table>"
+
+?>
